72678e33cf0b7c1b95ebc1f1b19f16a2faae23d4
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-08 13:39:46 -0300

    Create NewsRepository Class

---
+++ app/Repositories/ImageNews/ImageNewsRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Repositories\ImageNews;
+
+
+use App\Repositories\AbstractRepository;
+
+class ImageNewsRepository extends AbstractRepository
+{
+
+
+}

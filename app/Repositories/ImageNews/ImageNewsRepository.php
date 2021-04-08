2b927563795119d2234cec793a0d92d303853e2d
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-08 13:53:40 -0300

    Create ImageNewsRepository Class

--- app/Repositories/ImageNews/ImageNewsRepository.php
+++ app/Repositories/ImageNews/ImageNewsRepository.php
@@ -10,6 +10,24 @@ use App\Repositories\AbstractRepository;
 
 class ImageNewsRepository extends AbstractRepository
 {
+    /**
+     * @param int $newsId
+     * @return array
+     */
+    public function findByNews(int $newsId): array
+    {
+        return $this->model::where('noticia_id', $newsId)->get()->toArray();
+    }
+
+    /**
+     * @param int $newsId
+     * @return bool
+     */
+    public function deleteByNews(int $newsId): bool
+    {
+        $result = $this->model::where('noticia_id', $newsId)->delete();
+        return $result ? true : false;
+    }
 
 
 }
